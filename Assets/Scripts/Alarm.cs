7bd936e51d07b85561211a81c718b2331643cdcb
SlavBrain <82366112+SlavBrain@users.noreply.github.com>
2022-07-05 23:03:39 +0300

    v2

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -4,52 +4,52 @@ using UnityEngine;
 
 public class Alarm : MonoBehaviour
 {
-    private Security[] _homes;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private float _changingVolumeTime;
-    private bool _isIncreaseVolumeRunning = false;
-    private bool _isTurnDownVolumeRunning = false;
+
+    private Coroutine _changingVolume;
 
     public void TurnOnAlarm()
     {
-        StartCoroutine( IncreaseVolume());
+        if (_changingVolume!=null)
+        {
+            StopCoroutine(_changingVolume);
+        }
+
+        _changingVolume= StartCoroutine( IncreaseVolume());
     }
 
     public void TurnOffAlarm()
-    {        
-        StartCoroutine( TurnDownVolume());
+    {
+        if (_changingVolume != null)
+        {
+            StopCoroutine(_changingVolume);
+        }
+
+        _changingVolume = StartCoroutine( TurnDownVolume());
     }
 
     private IEnumerator IncreaseVolume()
     {
-        _isTurnDownVolumeRunning = false;
-        _isIncreaseVolumeRunning = true;
         float _changingVolumeRunningTime = 0;
 
-        while (_audioSource.volume < 1f&& _isTurnDownVolumeRunning==false)
+        while (_audioSource.volume < 1f)
         {
             _changingVolumeRunningTime += Time.deltaTime;
             _audioSource.volume = Mathf.Lerp(_audioSource.volume, 1f,_changingVolumeRunningTime/ _changingVolumeTime);
             yield return null;
         }
-
-        _isIncreaseVolumeRunning = false;
     }
 
     private IEnumerator TurnDownVolume()
     {
-        _isIncreaseVolumeRunning = false;
-        _isTurnDownVolumeRunning = true;
         float _changingVolumeRunningTime = 0;        
 
-        while (_audioSource.volume > 0f&& _isIncreaseVolumeRunning==false)
+        while (_audioSource.volume > 0f)
         {
             _changingVolumeRunningTime += Time.deltaTime;
             _audioSource.volume = Mathf.Lerp(_audioSource.volume, 0f, _changingVolumeRunningTime / _changingVolumeTime);
-            Debug.Log("Сирена уменьш " + _audioSource.volume + " " + _changingVolumeRunningTime);
             yield return null;
         }
-
-        _isTurnDownVolumeRunning = false;
     }
 }
--- Assets/Scripts/Security.cs
+++ Assets/Scripts/Security.cs
@@ -5,10 +5,8 @@ using UnityEngine.Events;
 
 public class Security : MonoBehaviour
 {
-    [SerializeField ]private UnityEvent _onThiefInHome;
+    [SerializeField] private UnityEvent _onThiefInHome;
     [SerializeField] private UnityEvent _onThiefOutHome;
-    private bool _isThiefInHome;
-    private bool _isThiefOutHome;
 
     public event UnityAction OnThiefInHome
     {
@@ -24,30 +22,16 @@ public class Security : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (_isThiefInHome)
-        {
-            return;
-        }
-
         if(collision.TryGetComponent<Player>(out Player player))
         {
-            _isThiefInHome=true;
-            _isThiefOutHome = false;
             _onThiefInHome?.Invoke();
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (_isThiefOutHome)
-        {
-            return;
-        }
-
         if (collision.TryGetComponent<Player>(out Player player))
         {
-            _isThiefInHome = false;
-            _isThiefOutHome = true;
             _onThiefOutHome?.Invoke();
         }
     }
